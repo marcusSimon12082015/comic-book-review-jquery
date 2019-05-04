14410d7d179c7eb5f46482a80fe2c4264102f304
Matija Simin <marcussimon12082015.hotmail.com>
2019-05-04 08:05:33 -0700

    adding methods to User class for third party authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,29 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :comic_reviews, :through => :reviews, :source => 'comic'
   #################################################################################
+
+  def firstEmailPart
+    email[0,email.index('@')]
+  end
+
+  def add_comic_to_user(comic)
+    if self.credits >= comic.price.to_i
+      self.comicsofUser << comic
+      self.credits -= comic.price.to_i
+      self
+    else
+      return nil
+    end
+  end
+
+  #for third party authentication
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+  ##############################################################################
 end

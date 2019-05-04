153012c4495b6ae36c41baa7decbcfb8085aa2c4
Matija Simin <marcussimon12082015.hotmail.com>
2019-05-04 10:47:34 -0700

    adding new and create actions to comic controller

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -7,6 +7,32 @@ class ComicsController < ApplicationController
     else
       @comics = Comic.all.customOrder('release_date')
     end
-  end 
+  end
 
+  def show
+  end
+
+  def create
+    @comic = Comic.new(comics_params)
+    authorize @comic
+    if @comic.save
+      respond_to do |format|
+        format.html { redirect_to @comic, success: 'Comic was successfully created!'}
+      end
+    else
+      render :action => 'new'
+    end
+  end
+
+  def new
+    @comic = Comic.new
+  end
+
+  def best_sellers
+    @comics = Comic.best_sellers
+  end
+
+  private
+
+  
 end

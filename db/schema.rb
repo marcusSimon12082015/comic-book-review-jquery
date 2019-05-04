3168b5af65b254172d15aadb7db84dbb53323406
Matija Simin <marcussimon12082015.hotmail.com>
2019-05-04 07:53:21 -0700

    adding seed data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,90 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_142117) do
+ActiveRecord::Schema.define(version: 2019_05_04_144741) do
+
+  create_table "artist_comics", force: :cascade do |t|
+    t.integer "comic_id"
+    t.integer "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_artist_comics_on_artist_id"
+    t.index ["comic_id"], name: "index_artist_comics_on_comic_id"
+  end
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.string "biography"
+    t.string "artist_image"
+    t.integer "type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type_id"], name: "index_artists_on_type_id"
+  end
+
+  create_table "character_comics", force: :cascade do |t|
+    t.integer "character_id"
+    t.integer "comic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["character_id"], name: "index_character_comics_on_character_id"
+    t.index ["comic_id"], name: "index_character_comics_on_comic_id"
+  end
+
+  create_table "characters", force: :cascade do |t|
+    t.integer "publisher_id"
+    t.string "name"
+    t.string "bio"
+    t.string "character_image"
+    t.index ["publisher_id"], name: "index_characters_on_publisher_id"
+  end
+
+  create_table "comics", force: :cascade do |t|
+    t.string "title"
+    t.date "release_date"
+    t.string "description"
+    t.integer "publisher_id"
+    t.float "price"
+    t.integer "pages"
+    t.float "rating", default: 0.0
+    t.string "comic_cover_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["publisher_id"], name: "index_comics_on_publisher_id"
+  end
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "comic_id"
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comic_id"], name: "index_reviews_on_comic_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
+  create_table "types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_comics", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "comic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comic_id"], name: "index_user_comics_on_comic_id"
+    t.index ["user_id"], name: "index_user_comics_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,60 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Create admin user
+User.create(email:'admin@admin.com',password:"1111111111",password_confirmation:"1111111111",
+role:1)
+
+
+#Creating Publishers
+marvel = Publisher.create(name:'Miracles');
+dc = Publisher.create(name:'DOUG Comics');
+tommorow = Publisher.create(name:'Tommorow Comics');
+
+#Creating Comics
+xmenComic = Comic.create(title:'Less-Men #1',description:'A',release_date:'15-08-2018',publisher: marvel,price:15.0,pages:20,rating:7);
+batmanComic = Comic.create(title:'Beatman #1',description:'B',release_date:'09-12-2018',publisher: dc,price:20.0,pages:25,rating:6);
+supermanComic = Comic.create(title:'Silent Man #1',description:'C',release_date:'06-08-2018',publisher: dc,price:15.0,pages:20,rating:5);
+ghostStories = Comic.create(title:'Ghost Stories #3',description:'D',release_date:'15-05-2018',publisher: tommorow,price:15.0,pages:30,rating:8);
+xmen2Comic = Comic.create(title:'Less-Men #2',description:'E',release_date:'15-09-2018',publisher: marvel,price:15.0,pages:20,rating:8);
+ghostStories2 = Comic.create(title:'Ghost Stories #4',description:'F',release_date:'15-10-2018',publisher: tommorow,price:15.0,pages:25,rating:4);
+spidermanComic = Comic.create(title:'Bananaman #3',description:'G',release_date:'10-08-2018',publisher: marvel,price:15.0,pages:20,rating:2);
+wonderWomanComic = Comic.create(title:'Giant Woman #1',description:'H',release_date:'10-05-2018',publisher: dc,price:20.0,pages:30,rating:10);
+
+#Creating Characters
+jinkser = Character.create(name:'Jinkser',bio:'Very sinister.',publisher:dc)
+beatman = Character.create(name:'Beatman',bio:'Very dark.',publisher:dc)
+silentman = Character.create(name:'Silentman',bio:'Report to silent',publisher:dc)
+lessman = Character.create(name:'Lessman',bio:'Peter',publisher:marvel)
+talia = Character.create(name:'Talia',bio:'Warior',publisher:tommorow)
+blueBird = Character.create(name:'Blue Bird',bio:'Bad guy',publisher:tommorow)
+ironMan = Character.create(name:'Ilon Man',bio:'Rich',publisher:marvel)
+greyWorm = Character.create(name:'Grey Worm',bio:'Special',publisher:tommorow)
+
+#Adding characters to comics
+batmanComic.characters << jinkser
+batmanComic.characters << beatman
+
+talia.comics << [ghostStories, ghostStories2]
+
+supermanComic.characters << silentman
+
+lessman.comics << spidermanComic
+
+
+#Type of Artist
+writer = Type.create(name:'Writer')
+colorist = Type.create(name:'Colorist')
+coverArtist = Type.create(name:'Cover Artist')
+letterer = Type.create(name:'Letterer')
+editor = Type.create(name:'Editor')
+assistantEditor = Type.create(name:'Assistant Editor')
+
+#Artists
+john = Artist.create(name:'John',surname:'Snow',biography:'Green',type:colorist)
+dale = Artist.create(name:'Dale',surname:'Sure',biography:'Fun',type:coverArtist)
+lucy = Artist.create(name:'Lucy',surname:'Lu',biography:'Fun',type:letterer)
+frank = Artist.create(name:'Frank',surname:'Lith',biography:'Fun',type:writer)
+lit = Artist.create(name:'Lit',surname:'Hawk',biography:'Fun',type:editor)
+joe = Artist.create(name:'Joe',surname:'Frjk',biography:'Fun',type:assistantEditor)

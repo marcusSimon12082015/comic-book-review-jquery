938d4731b2f4f3df90dfbf89b917254d63adba1a
Matija Simin <marcussimon12082015.hotmail.com>
2019-05-04 08:27:28 -0700

    adding validations to Comic class

--- app/models/comic.rb
+++ app/models/comic.rb
@@ -8,5 +8,25 @@ class Comic < ApplicationRecord
   has_many :character_comics
   has_many :characters, :through => :character_comics
 
-  
+  has_many :artist_comics
+  has_many :artists, :through => :artist_comics
+  accepts_nested_attributes_for :artists, reject_if:
+    lambda {|attributes| attributes['name'].blank? || attributes['surname'].blank? ||
+      attributes['type_id'].blank?}
+
+  has_many :user_comics, dependent: :destroy
+  has_many :users_comics, :through => :user_comics, :source => "user"
+
+  has_many :reviews
+  has_many :user_reviews, :through => :reviews, :source => "user"
+  ###############################################################################
+
+  #validations
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true, numerically: {:greater_than => 0.0}
+  validates :pages, presence: true, numerically: {:greater_than => 0.0}
+  validates_associated :artists
+  ###########################################################################
+
 end

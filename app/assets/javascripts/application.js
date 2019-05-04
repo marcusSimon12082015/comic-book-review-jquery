5b19b9a309b0b095e16978ed5ee8cf792e860167
Matija Simin <marcussimon12082015.hotmail.com>
2019-05-04 09:04:17 -0700

    adding references of javascript files to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
+//= require script
+//= require toastr
+//= require nested_form_fields

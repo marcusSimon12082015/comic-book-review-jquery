37a133e108e834f7b6d40234f8212f37be0cf42a
Matija Simin <marcussimon12082015.hotmail.com>
2019-05-04 09:46:08 -0700

    creating comics show page

--- app/helpers/comic_helper.rb
+++ app/helpers/comic_helper.rb
@@ -1,4 +1,20 @@
 module ComicHelper
+  def selectedPublisher(search)
+    if !search.nil? && search.key?(:publisher_id)
+      return search[:publisher_id]
+    else
+      return 0
+    end
+  end
+
+  def selectedOrder(search)
+    if !search.nil? && search.key?(:sort_order)
+      return search[:sort_order]
+    else
+      return 1
+    end
+  end
+
   def displayCoverImage(comic)
     comic.comic_cover_image.file.nil? ? "/assets/cover_not_available_360.jpg"
                                       : comic.comic_cover_image.url.to_s

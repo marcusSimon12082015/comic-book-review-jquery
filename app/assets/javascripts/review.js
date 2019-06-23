5a7d993b3080bf0c070fecf4c42f8b632aa502fe
Matija Simin <marcussimon12082015.hotmail.com>
2019-06-23 08:28:59 -0700

    added review javascript class with constructor

---
+++ app/assets/javascripts/review.js
@@ -0,0 +1,11 @@
+class Review{
+  constructor(id,title,content,userEmail,next,previous)
+  {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.userEmail = userEmail;
+    this.next = next;
+    this.previous = previous;
+  }
+}

441b447bdbd0e1b214000a8adcd548fb248d5a5e
Matija Simin <marcussimon12082015.hotmail.com>
2019-06-23 08:39:56 -0700

    added next_link method to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ApplicationRecord
   include Order
   include Rails.application.routes.url_helpers
-  
+
   #validations
   validates :title, presence: true
   validates :content, presence: true
@@ -16,4 +16,22 @@ class Review < ApplicationRecord
   def self.searchByComic(id)
     where('comic_id = ?',id).customOrder('created_at')
   end
+
+  def next_link(comic_id=0)
+    if comic_id > 0
+      @review = Review.where('comic_id = ? and created_at < ?',comic_id,created_at).last
+      if @review.nil?
+        return ""
+      else
+        return comic_review_path(comic_id,@review.id)
+      end
+    else
+      @review = Review.where('created_at < ?',created_at).last
+      if @review.nil?
+        return ""
+      else
+        return review_path(@review)
+      end
+    end
+  end 
 end

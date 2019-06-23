daa3be7d8633b54b6a4e2f1ccc18e159476a4b0a
Matija Simin <marcussimon12082015.hotmail.com>
2019-06-23 09:23:48 -0700

    added renderList method to comment.js

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -4,4 +4,9 @@ class Comment{
     this.content = content;
     this.userEmail = userEmail;
   }
+
+  renderListItem()
+  {
+    return '<li class="comment-panel"><p>'+this.userEmail+'</p><p>'+this.content+'</p></li>';
+  }
 }
